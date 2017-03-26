b9cd1bd5f3f49ee5fc60aac67e5c11e748ef3b3a
Robert Boyett <rdboyett@gmail.com>
2017-03-25 23:36:18 -0500

    corrected static path

--- workshop_project/settings.py
+++ workshop_project/settings.py
@@ -121,7 +121,7 @@ ADMIN_MEDIA_PREFIX = '/media/admin/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/workshop/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..','static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
